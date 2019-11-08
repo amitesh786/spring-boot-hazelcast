520e5e4386f94f51ec9b433d0d2260ad2a7cba6c
Amitesh Singh <amitesh.singh@corvallis.it>
2019-11-08 16:31:34 +0100

    [change] bump snapshot version

--- mvn-version.sh
+++ mvn-version.sh
@@ -2,10 +2,18 @@
 mvn clean install package -X
 
 # Maven parent module version update
-mvn versions:set -DnewVersion=1.0.0
+mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
 
 # Maven sub module version update
-mvn versions:update-property -Dproperty=hazelcast-node.version -DnewVersion=1.0.0
+# mvn versions:update-property -Dproperty=hazelcast-node.version -DnewVersion=1.0.1-SNAPSHOT
+
+cd hazelcast-node
+mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+cd ../
 
 # Maven sub module version update
-mvn versions:update-property -Dproperty=spring-hazelcast.version -DnewVersion=1.0.0
+#mvn versions:update-property -Dproperty=spring-hazelcast.version -DnewVersion=1.0.1-SNAPSHOT
+
+cd spring-hazelcast
+mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+cd ../
