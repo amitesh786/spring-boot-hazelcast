88d484c6ade96ad25b56b83e46cf3bc414d0b800
Amitesh Singh <amitesh.singh@corvallis.it>
2019-11-14 10:21:48 +0100

    [change] release 2.0.0 version update

--- mvn-version.sh
+++ mvn-version.sh
@@ -2,18 +2,18 @@
 mvn clean install package -X
 
 # Maven parent module version update
-mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+mvn versions:set -DnewVersion=2.0.0
 
 # Maven sub module version update
 # mvn versions:update-property -Dproperty=hazelcast-node.version -DnewVersion=1.0.1-SNAPSHOT
 
 cd hazelcast-node
-mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+mvn versions:set -DnewVersion=2.0.0
 cd ../
 
 # Maven sub module version update
 #mvn versions:update-property -Dproperty=spring-hazelcast.version -DnewVersion=1.0.1-SNAPSHOT
 
 cd spring-hazelcast
-mvn versions:set -DnewVersion=1.0.1-SNAPSHOT
+mvn versions:set -DnewVersion=2.0.0
 cd ../
